59d3fb7d11046e3301c68cedbee88a3b74817dbe
Lakshmi-Sunil <lakshmisunil12@gmail.com>
2019-04-04 23:52:06 +0530

    Admin Interface

---
+++ includes/js/common.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+	$('#hdr-menu-btn').click(function(){
+		$('.main_left').show();
+		$('.main_right').hide();
+		$('.main_mid').hide();
+	});
+});
---
+++ includes/js/nav.js
@@ -0,0 +1,21 @@
+var nav=
+'<div class="main_left" >\
+	<a href="#" class="logo" ><img src="includes/images/logome.jpg" /></a>\
+		<div class="main_menu" >\
+			<ul>\
+				<li>\
+					<div><i class="fas fa-book"></i>Section 1</div>\
+					<ul>\
+						<li><a href="login.html" >Sign in</a></li>\
+						<li><a href="residents list.html" >Residents list</a></li>\
+						<li><a href="tickpaid.html" >Payment</a></li>\
+						<li><a href="whoallpaid.html" >View payment history</a></li>\
+						<li><a href="building details.html" >Building details</a></li>\
+						<li><a href="page-Copy.html" >Roles and responsibilities</a></li>\
+						<li><a href="page-Copy.html" >Setup</a></li>\
+					</ul>\
+				</li>\
+			<ul>\
+		</div>\
+</div>';
+document.write(nav);
